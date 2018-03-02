4b32e7fcbe32cb332a5dfa2b4c038731dafd96bc
nfsrun <kevin.tran@bellevuecollege.edu>
2018-03-01 20:00:40 -0800

    Chosen files for final upload and organized files. Added some changes to code for sound. Still need to add more options and sounds.

---
+++ Final/flappy_400x500.py
@@ -0,0 +1,168 @@
+import pygame
+from pipe import Pipe
+from bird import Bird
+import random
+
+pygame.init()
+
+black = (0, 0, 0)
+red = (255, 0, 0)
+bg = pygame.image.load("bkgrnd.png")
+startscreen = pygame.image.load("startscreen_400x500.png")
+flappy = Bird()
+
+bgm = pygame.mixer.Sound("bgm.wav")
+
+
+all = pygame.sprite.Group()
+all.add(flappy)
+
+highScore = 0
+
+size = 400, 500
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Flappy Bird")
+
+done = False
+clock = pygame.time.Clock()  # will setup frames per second
+
+allPipes = pygame.sprite.Group()  # the pipe.py sprites for the game
+
+# define coordinates
+# start position of flappy
+x = 150
+y = 250
+#speed of
+x_speed = 0
+y_speed = 0
+# mark ground
+ground = 495
+#position to start drawing  pipes
+xloc = 500
+#passage for bird between pipes  will  be randomized per pass
+
+space = random.randint(80, 100)
+ready = False
+yloc = random.randint(32, 400-32-space) # we will determine this by a random call
+
+# size of the pipes LxW
+xsize = 100
+ysize = random.randint(100, 325)
+
+# speed to draw a pipe, make pipe visual on screen
+pipespeed = 4.0
+points = 0
+lastKey = 0
+
+bottomPipe = Pipe(1, xloc, yloc, space)
+topPipe = Pipe(0, xloc, bottomPipe.rect.y, space)
+
+allPipes.add(topPipe) #IMPORTANT THIS GOES FIRST LOOOOL OR IT WONT CALCULATE IT CORRECTLY OMG
+allPipes.add(bottomPipe)
+
+pygame.draw.rect(screen, black, [0, 300, 400, 60])
+
+# start game screen
+start = False
+while not start:
+    pygame.display.flip()
+    screen.blit(startscreen, [0, 0])
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if mouse_x>156 and mouse_x< 241 and mouse_y>319 and mouse_y<352:
+                start = True
+pygame.display.flip()
+
+def gameOver():  # check if the bird goes too far down
+    bgm.stop()
+    font = pygame.font.Font(None, 30)
+    text = font.render("Game over", True, red)
+    screen.blit(text, [200, 350])
+    pygame.display.flip()
+    pygame.time.wait(6000)
+
+def readyScreen():
+    font = pygame.font.Font(None, 30)
+    text = font.render("Press up to start", True, red)
+    screen.blit(text, [200, 350])
+    pygame.display.flip()
+
+def score(points):
+    font = pygame.font.Font(None, 20)
+    text = font.render("Score: " + str(points), True, black)
+    screen.blit(text, [1, 1])
+
+
+
+screen.blit(bg, [0, 0])
+allPipes.draw(screen)
+screen.blit(flappy.image, [150, 250])
+score(points)
+readyScreen()
+
+while not ready:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:  # to start the game / control
+            y_speed = -10
+            lastKey = pygame.K_UP
+            ready = True
+            bgm.play()
+while not done and start:
+
+    for event in pygame.event.get():  # game interaction
+
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.KEYDOWN:  # to start the game / control
+            if event.key == pygame.K_UP:
+                y_speed = -10
+                lastKey = pygame.K_UP
+            elif event.key == pygame.K_w:
+                y_speed = -10
+                lastKey = pygame.K_w
+            elif event.key == pygame.K_SPACE:
+                y_speed = -10
+                lastKey = pygame.K_SPACE
+        if event.type == pygame.KEYUP:
+            if event.key == lastKey:  # release key
+                y_speed = 5
+    screen.blit(bg, [0, 0])
+    flappy.update(x, y)
+
+    allPipes.update(xloc, yloc, space)
+
+    all.draw(screen)
+    allPipes.draw(screen)
+    score(points)
+
+    for p in allPipes: #for each on all pipes instead
+        if pygame.sprite.collide_mask(flappy, p) != None: #change to check via collide mask for more accurate check on bird
+            done = True  # quit the whole program
+            gameOver()
+    y += y_speed/2 # what does this do?
+    xloc -= pipespeed
+
+    if y >= ground:
+        gameOver()
+        y_speed = y_speed/2
+        pipespeed = 0
+
+        done = True
+
+    if xloc < -80: # 80 px from the last pipe, draw the next one
+        xloc =400  # reset, x coordinate of drawing next pipe
+
+        ysize = random.randint(100, 200)  # reset gap between
+
+        space = random.randint(80, 100)  #randomize the gap length
+        random.seed()
+        yloc = random.randint(32, 400-space-32) #randomize the pipe placements
+#or xloc== 0
+    if x > xloc and x < xloc + 5 : # made it past the pipe
+        points = (points + 1)
+
+    pygame.display.flip()
+    clock.tick(60)
+
+quit();
---
+++ Work/flappybird1.py
@@ -0,0 +1,117 @@
+import pygame
+from random import randint
+
+pygame.init()
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+green = (59, 204, 100)
+bg = pygame.image.load("bkgrnd.png")
+flappy = pygame.image.load("flappy.png")
+
+size = 700, 500
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Flappy Bird")
+
+done = False
+clock = pygame.time.Clock()  # will setup frames per second
+
+
+# the bird
+def bird(x, y):
+    screen.blit(flappy, [x, y])
+
+
+def gameOver():  # check if the ball goes too far down
+
+    font = pygame.font.SysFont("comicsansms.tff", 20)
+    text = font.render("Game over", True, black)
+    screen.blit(text, [150, 250])
+    pygame.display.flip()
+    pygame.time.wait(6000)
+
+def pipes(xloc, yloc, xsize, ysize):
+    pygame.draw.rect(screen, green, [xloc, yloc, xsize, ysize])  # top rectangle
+    pygame.draw.rect(screen, green, [xloc, int(yloc + ysize + space), xsize, ysize + 500])
+
+
+def score(points):
+    font = pygame.font.SysFont("comicsansms.tff", 20)
+    text = font.render("Score: " + str(points), True, black)
+    screen.blit(text, [0, 0])
+
+
+# define x and y
+x = 350
+y = 250
+x_speed = 0
+y_speed = 0
+ground = 495  # 500 pi - redius of ball
+xloc = 700
+yloc = 0
+xsize = 70
+ysize = randint(0, 350)
+space = 150
+pipespeed = 2.5
+points = 0
+lastKey = 0
+# main program loop
+while not done:
+    for event in pygame.event.get():  # game interaction
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.KEYDOWN:  # to start the game?
+            if event.key == pygame.K_UP:
+                y_speed = -10
+                lastKey = pygame.K_UP
+            elif event.key == pygame.K_w:
+                y_speed = -10
+                lastKey = pygame.K_w
+            elif event.key == pygame.K_SPACE:
+                y_speed = -10
+                lastKey = pygame.K_SPACE
+        if event.type == pygame.KEYUP:
+            if event.key == lastKey:  # release key
+                y_speed = 5
+
+                #   screen.fill(white)
+    screen.blit(bg, [0, 0])
+    pipes(xloc, yloc, xsize, ysize)
+    bird(x, y)
+    score(points)
+
+    y += y_speed
+    xloc -= pipespeed
+
+    if y > ground:
+        gameOver()
+        y_speed = 0
+        pipespeed = 0  # stop screen movement
+
+    # hit the pipe.py condition
+    if x + 2 > xloc and y - 20 < ysize and x - 15 < xsize + xloc:  # upper pipe.py
+        gameOver()
+        y_speed = 0
+        pipespeed = 0
+        done = True
+
+    if x + 2 > xloc and y + 20 > ysize + space and x - 15 < xsize + xloc:  # lower pipe.py
+        gameOver()
+        y_speed = 0
+        pipespeed = 0
+        done = True
+
+    if xloc < -80:
+        xloc = 700  # reset
+        ysize = randint(50, 350)  # reset gap between pipes
+
+    if x > xloc and x < xloc + 3:
+        points = (points + 1)
+
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()  # quit the whole program
+
+
+
